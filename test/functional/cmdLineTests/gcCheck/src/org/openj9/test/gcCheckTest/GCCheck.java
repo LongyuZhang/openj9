635e2248359a7972438affa0c43b4338e4b16d55
Longyu Zhang <longyu.zhang@ibm.com>
2021-11-29 21:34:56 -0500

    Fix GCCheck fialure on plinux machine - Fix class not found exception - Related Issue: [skip ci]
    Signed-off-by: Longyu Zhang <longyu.zhang@ibm.com>

--- test/functional/cmdLineTests/gcCheck/src/org/openj9/test/gcCheckTest/GCCheck.java
+++ test/functional/cmdLineTests/gcCheck/src/org/openj9/test/gcCheckTest/GCCheck.java
@@ -20,9 +20,9 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
-package org.openj9.test.nogc;;
+package org.openj9.test.gcCheckTest;
 
-class Main {
+public class GCCheck {
 	public static void main(String args[]) throws Exception {
 		int count = 0;
  	  	Thread allocator = new Allocator();
